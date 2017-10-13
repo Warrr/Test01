639f341dd08a074ea077507b78f151aec3c82478
War <428384851@qq.coom>
2017-10-14 00:06:08 +0800

    the first change

--- test01/test01.cpp
+++ test01/test01.cpp
@@ -4,8 +4,35 @@
 #include "stdafx.h"
 
 
-int main()
+#include <stdio.h>
+#include <stdlib.h>
+int main(void)
 {
-    return 0;
-}
+	float amount, rate, mpay, sum1, sum2, sum3, m_rate;
+	printf("enter amount if loan:\n");		//输入应还款金额
+	scanf_s("%f", &amount);
+	printf("enter interest rate:\n");		//输入年利率
+	scanf_s("%f", &rate);
+	printf("enter monthly payment:\n");		//输入每月还款金额
+	scanf_s("%f", &mpay);
 
+	m_rate = rate / 1200;
+	printf("monthly rate = %f\n", m_rate);
+
+	sum1 = (amount - mpay)*(m_rate + 1.0);
+	sum2 = (sum1 - mpay)*(m_rate + 1.0);
+	sum3 = (sum2 - mpay)*(m_rate + 1.0);
+
+	printf("sum1 = %f\n", sum1);
+	printf("sum2 = %f\n", sum2);
+	printf("sum3 = %f\n", sum3);
+
+	printf("this is the first change\n\n\n\n");
+
+	system("pause");
+
+
+
+
+
+}
